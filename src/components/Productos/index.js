a7c8e17cd194930a07517f6f8dcc794760ae395c
Jorge Romo <andresrt952@gmail.com>
2023-06-01 10:45:41 -0500

    correccion de imagenes

--- src/components/Productos/index.js
+++ src/components/Productos/index.js
@@ -1,39 +1,38 @@
-import './productos.css';
-import { useState, useEffect } from "react";
-import ImagenesApi from "./../ImagenesApi";
+import './productos.css'
+
 import { buscar } from "../../api/api";
+import { useState, useEffect } from "react";
 
 const ListarProductos = ({ url }) => {
   const [productos, setProductos] = useState([]);
-
+  const [imagenes, setImagenes] = useState([]);
+//mejorar esto para que no se haga la peticion cada vez que se renderiza
   useEffect(() => {
     buscar(url, setProductos);
+    //crear un nuevo componente para las imagenes. luegoooo!
+    buscar('https://apisdelta.azurewebsites.net/api/imagenes/', setImagenes);
   }, [url]);
 
   return (
-    <ImagenesApi url="/api/imagenes">
-      {(imagenes) => (
-        <div>
-          <div className="container">
-            {productos.map((producto) => (
-              <div className="card" key={producto.id}>
-                <img src={obtenerImagen(imagenes, producto.idImagen)} alt={producto.nombre} />
-                <h4>{producto.nombre}</h4>
-                <p>{producto.descripcion}</p>
-                <p>S/.{producto.precio}</p>
-                <button>Comprar</button>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-    </ImagenesApi>
-  );
-};
+    <div>
+      <div className="container">
+        {productos.map((producto) => {
+          const imagen = imagenes.find((img) => img.id === producto.imagen);
+          const imagenURL = imagen ? imagen.nombre : '';
 
-const obtenerImagen = (imagenes, idImagen) => {
-  const imagen = imagenes.find((imagen) => imagen.id === idImagen);
-  return imagen ? imagen.nombre : '';
-};
+          return (
+            <div className="card" key={producto.id}>
+              <img src={imagenURL} alt={producto.nombre} />
+              <h4>{producto.nombre}</h4>
+              <p>{producto.descripcion}</p>
+              <p>S/.{producto.precio}</p>
+              <button className="btn-comprar">Comprar</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default ListarProductos;
