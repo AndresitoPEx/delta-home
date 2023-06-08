55087767b7129bf05d8f6fbb1309f592f28513f1
Jorge Romo <andresrt952@gmail.com>
2023-06-07 21:52:13 -0500

    preparando para el sprint 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,34 +8,34 @@ const Header = () => {
 
     const activeStyle = {
         color: '#F5821F',
-        borderbottom: '#F5821F 2px solid'
 
-};
+    };
 
 
-return (
-    <header className="header">
-        <div className='head-container'>
-            <div className='space'>
-                <p></p>
+    return (
+        <header className="header">
+            <div className='head-container'>
+                <div className='space'>
+                    <p></p>
+                </div>
+                <Link to='/' className='logo'>
+                    <img src="./img/delta-logo.png" alt="logo" />
+                </Link>
             </div>
-            <Link to='/' className='logo'>
-                <img src="./img/delta-logo.png" alt="logo" />
-            </Link>
-        </div>
-        <nav>
-            <ul className='options'>
-                <li><Link className='link' to="/" style={location.pathname === '/' ? activeStyle : {}}>Inicio</Link></li>
-                <li><Link className='link' to="/productos" style={location.pathname === '/productos' ? activeStyle : {}}>Productos</Link></li>
-                <li><Link className='link' to="/nosotros" style={location.pathname === '/nosotros' ? activeStyle : {}}>Nosotros</Link></li>
-                <li><Link className='link' to="/contacto" style={location.pathname === '/contacto' ? activeStyle : {}}>Contacto</Link></li>
-                <li><Link className='link' to="/login" style={location.pathname === '/login' ? activeStyle : {}}>Login</Link></li>
-                <li><Link className='link' to="/registrar-usuario" style={location.pathname === '/registrar-usuario' ? activeStyle : {}}>Registrate</Link></li>
-            </ul>
-        </nav>
+            <nav>
+                <ul className='options'>
+                    <li><Link className='link' to="/" style={location.pathname === '/' ? activeStyle : {}}>Inicio</Link></li>
+                    <li><Link className='link' to="/productos" style={location.pathname === '/productos' ? activeStyle : {}}>Productos</Link></li>
+                    <li><Link className='link' to="/nosotros" style={location.pathname === '/nosotros' ? activeStyle : {}}>Nosotros</Link></li>
+                    <li><Link className='link' to="/contacto" style={location.pathname === '/contacto' ? activeStyle : {}}>Contacto</Link></li>
+                    <li><Link className='link' to="/login" style={location.pathname === '/login' ? activeStyle : {}}>Login</Link></li>
+                    <li><Link className='link' to="/registrar-usuario" style={location.pathname === '/registrar-usuario' ? activeStyle : {}}>Registrate</Link></li>
+                </ul>
+            </nav>
+            
 
-    </header>
-)
+        </header>
+    )
 
 
 }
